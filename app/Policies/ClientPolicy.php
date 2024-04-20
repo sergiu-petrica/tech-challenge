0dfa113bc5512022400ac00b58b3eef46aba519c
Sergiu <sergiu.petrica91@gmail.com>
2024-04-20 17:18:22 +0300

    Fixed bug where accessing clients the user has no access to would crash

--- app/Policies/ClientPolicy.php
+++ app/Policies/ClientPolicy.php
@@ -12,16 +12,25 @@ class ClientPolicy
 
     public function view(User $user, Client $client): bool
     {
-        return $client->user->is($user);
+        return $this->belongsToUser($user, $client);
     }
 
     public function update(User $user, Client $client): bool
     {
-        return $client->user->is($user);
+        return $this->belongsToUser($user, $client);
     }
 
     public function destroy(User $user, Client $client): bool
     {
+        return $this->belongsToUser($user, $client);
+    }
+
+    public function belongsToUser(User $user, Client $client): bool
+    {
+        if (!$client->user) {
+            return false;
+        }
+
         return $client->user->is($user);
     }
 }
